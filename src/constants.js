9055fac9e0979da945b71e9d2717f66d1c02528b
suresh <sureshamahendra.gmail.com>
2022-03-19 23:08:48 +0530

    firstcomit

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const AppName =  "Music-app";
+export const AppDesc =  "Feel and Heal 💊";
+export const NAPSTER_APP_ID = "aede99f7-f71a-4ca4-acc5-f7a09c6a01e6";
+export const NAPSTER_API_KEY = "MTFiYjEyZDMtOTE4ZC00Y2JlLWIyMjktODVkMGNlMDcyODFk";
+export const NAPSTER_API_SECRET = "MzkyMTI2MmUtYzAzNC00MmEyLWEzMDMtMzI2MDExNzFmMmEy";
+
+
+export const SET_PLAY_NOW     = "SET_PLAY_NOW";
+export const MAKE_FAVOURITE   = "MAKE_FAVOURITE";
+export const REMOVE_FAVOURITE = "REMOVE_FAVOURITE";
+export const ADD_TO_PLAYLIST = "ADD_TO_PLAYLIST";
+export const REMOVE_FROM_PLAYLIST = "REMOVE_FROM_PLAYLIST";
+export const PLAY_NEXT_SONG = "PLAY_NEXT_SONG";
+export const PLAY_PREV_SONG = "PLAY_PREV_SONG";
+export const REPLACE_PLAYLIST = "REPLACE_PLAYLIST";
